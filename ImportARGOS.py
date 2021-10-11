1a226dd1e01f8de6f8389f6a3312730a17c4678f
Francesca Chiappetta <57842568+fachiappetta@users.noreply.github.com>
2021-10-10 22:33:48 -0400

    Create ImportARGOS.py

---
+++ ImportARGOS.py
@@ -0,0 +1,11 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2020
+## Author: John.Fay@duke.edu (for ENV859)
+##---------------------------------------------------------------------
